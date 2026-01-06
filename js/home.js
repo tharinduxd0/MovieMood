1ae9827fd60c4cf45a8759d4f1b0ea9c9247cdc8
KAinoshi <s22010235@ousl.lk>
2026-01-06 12:14:22 +0530

    added js for auth

---
+++ js/home.js
@@ -0,0 +1,12 @@
+function signup(e) {
+  e.preventDefault();
+  alert("Sign Up Successful! (Demo)");
+  window.location.href = "signin.html";
+}
+
+function signin(e) {
+  e.preventDefault();
+  alert("Sign In Successful! (Demo)");
+  // redirect to home page
+  window.location.href = "index.html";
+}
