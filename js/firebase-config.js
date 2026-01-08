43d4bb8921ef6e69a7b40260522f2423272db657
Tharindu D Nayanajith <info.tharinduxd@gmail.com>
2026-01-08 12:38:38 +0530

    Integrate Firebase SDK and configuration

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGZC3CXXMGGILhEUlgVDn6wpT7MTMTJbM",
+  authDomain: "moviemood-b1378.firebaseapp.com",
+  projectId: "moviemood-b1378",
+  storageBucket: "moviemood-b1378.firebasestorage.app",
+  messagingSenderId: "431597643962",
+  appId: "1:431597643962:web:ebaa6e2f5faaf5f1daba83"
+};
+
+const app = initializeApp(firebaseConfig);
